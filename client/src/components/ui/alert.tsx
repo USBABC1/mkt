ebdc9b6562b49ea5ab3e27a6446f381c8f03ec67
USBABC1 <contatousbabc@gmail.com>
2025-06-12 23:15:11 -0300

    Update alert.tsx

--- client/src/components/ui/alert.tsx
+++ client/src/components/ui/alert.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
+import { AlertTriangle, Terminal } from "lucide-react" // <-- "AlertTriangle" foi adicionado aqui
 
 import { cn } from "@/lib/utils"
 
